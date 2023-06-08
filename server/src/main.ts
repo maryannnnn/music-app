5b2f3e4b56d66d3f1095781c7571d9b9370de6e1
maryannnnn <info@neo-lines.com>
2023-06-09 00:39:24 +0300

    Defined parameters environment

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function start() {
-  const PORT = process.env.PORT
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT, () => console.log(`Server started on port =${PORT}`));
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+
+  const PORT = configService.get('PORT')
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
 
 start();
