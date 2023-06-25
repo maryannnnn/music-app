70dfa6cefc45936dc4f1a9e90f6e33e157f15d1d
maryannnnn <info@neo-lines.com>
2023-06-25 21:21:34 +0300

    Add Storybook and Seted Nrxt.js

--- server/src/auth/jwt-auth.guard.ts
+++ server/src/auth/jwt-auth.guard.ts
@@ -1,20 +1,22 @@
 import {CanActivate, ExecutionContext, Injectable, UnauthorizedException} from "@nestjs/common";
 import {AuthService} from "./auth.service";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
     constructor(private authService: AuthService) {
     }
 
-    async canActivate(context: ExecutionContext): Promise<boolean> {
-        const req= context.switchToHttp().getRequest()
-        const [bearer, token] = req.headers.authorization?.split(' ') ?? []
-
-        if (bearer !== 'Bearer' || !token) {
-            throw new UnauthorizedException({message: 'User not authorization'})
-        }
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         try {
-            const {user} = this.authService.verifyToken(token);
+            const req = context.switchToHttp().getRequest()
+            const [bearer, token] = req.headers.authorization?.split(' ') ?? []
+
+            if (bearer !== 'Bearer' || !token) {
+                throw new UnauthorizedException({message: 'User not authorization'})
+            }
+
+            const user = this.authService.verifyToken(token);
             req.user = user;
             return true;
         } catch (e) {
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -4,6 +4,8 @@ import {UsersService} from "./users.service";
 import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {Users} from "./users.entity";
 import {JwtAuthGuard} from "../auth/jwt-auth.guard";
+import {Roles} from "../auth/roles-auth.decorator";
+import {RolesGuard} from "../auth/roles.guard";
 
 @ApiTags('Users')
 @Controller('users')
@@ -20,7 +22,9 @@ export class UsersController {
 
     @ApiOperation({summary: 'Get all users'})
     @ApiResponse({status: 200, type: [Users]})
-    @UseGuards(JwtAuthGuard)
+    //@UseGuards(JwtAuthGuard)
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
     @Get()
     getAll() {
         return this.usersService.getAllUsers()
