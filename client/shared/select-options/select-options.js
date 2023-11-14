d7c4973c7516457b350d3f7a12f68204ffd28e29
maryannnnn <info@neo-lines.com>
2023-11-15 01:48:58 +0300

    Created file config for material-ui

--- client/shared/select-options/select-options.js
+++ client/shared/select-options/select-options.js
@@ -22,7 +22,15 @@ export const selectIcons = [
     {label: 5, value: 'Pinterest'},
     {label: 6, value: 'AdminPanelSettings'},
     {label: 7, value: 'MenuBook'},
-    {label: 8, value: ''},
-    {label: 9, value: ''},
-    {label: 10, value: ''}
+    {label: 8, value: 'PermMedia'},
+    {label: 9, value: 'Category'},
+    {label: 10, value: 'PriceChange'},
+    {label: 11, value: 'ShoppingBasket'},
+    {label: 12, value: 'Storefront'},
+    {label: 13, value: 'ShoppingCartCheckout'},
+    {label: 14, value: 'LocalShipping'},
+    {label: 15, value: 'Comment'},
+    {label: 16, value: 'Payments'},
+    {label: 17, value: 'People'},
+    {label: 18, value: 'AddCircle'},
 ]
