7dddf9cc07fe204a2fdc0bd2c4db6e63b9f29726
maryannnnn <info@neo-lines.com>
2024-02-15 01:03:29 +0200

    Added upload files on server

--- server/src/file/file.controller.ts
+++ server/src/file/file.controller.ts
@@ -1,22 +1,23 @@
-import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
-import {Meta} from "../meta/meta.entity";
-import {UpdateMetaDto} from "../meta/dto/update-meta.dto";
-import {ApiOperation, ApiResponse} from "@nestjs/swagger";
-import {CreateMetaDto} from "../meta/dto/create-meta.dto";
-import {File} from "./file.entity";
-import {FileService} from "./file.service";
-import {CreateFileDto} from "./dto/create-file.dto";
-import {UpdateFileDto} from "./dto/update-file.dto";
+import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { UpdateMetaDto } from "../meta/dto/update-meta.dto";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { CreateMetaDto } from "../meta/dto/create-meta.dto";
+import { File } from "./file.entity";
+import { FileService } from "./file.service";
+import { UpdateFileDto } from "./dto/update-file.dto";
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { NewFileDto } from "./dto/new-file.dto";
 
-@Controller('file')
+@Controller('files')
 export class FileController {
     constructor(private fileService: FileService) {}
 
     @ApiOperation({summary: 'Create File'})
     @ApiResponse({status: 200, type: File})
-    @Post('/new')
-    createFile(@Body() dtoFile: CreateFileDto, dtoMeta: CreateMetaDto) {
-        return this.fileService.createFile(dtoFile, dtoMeta)
+    @Post('/upload')
+    @UseInterceptors(FilesInterceptor('files'))
+    uploadFile(@UploadedFiles() files: Express.Multer.File[], @Body() newDtoFile: NewFileDto[], @Body() dtoMeta: CreateMetaDto[]) {
+        return this.fileService.uploadFiles(files, newDtoFile, dtoMeta)
     }
 
     @ApiOperation({summary: 'Get File'})
@@ -36,10 +37,8 @@ export class FileController {
     @ApiOperation({summary: 'Update File'})
     @ApiResponse({status: 200, type: File})
     @Put('/edit')
-    updateFileById(@Body() updateFileDto: UpdateFileDto, updateMetaDto: UpdateMetaDto) {
+    updateFileById(@Body() updateFileDto: UpdateFileDto, @Body() updateMetaDto: UpdateMetaDto) {
         return this.fileService.updateFileById(updateFileDto, updateMetaDto)
     }
-
-
-
 }
+
--- server/src/file/file.service.ts
+++ server/src/file/file.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import {UpdateMetaDto} from "../meta/dto/update-meta.dto";
 import {InjectRepository} from "@nestjs/typeorm";
 import {CreateMetaDto} from "../meta/dto/create-meta.dto";
@@ -7,31 +7,119 @@ import {File} from "./file.entity";
 import {CreateFileDto} from "./dto/create-file.dto";
 import {MetaService} from "../meta/meta.service";
 import {UpdateFileDto} from "./dto/update-file.dto";
-import {UpdateMenuDto} from "../menu/dto/update-menu.dto";
+import * as sharp from 'sharp';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as uuid from 'uuid'
 import {Meta} from "../meta/meta.entity";
+import {NewFileDto} from "./dto/new-file.dto";
 
 @Injectable()
 export class FileService {
     constructor(@InjectRepository(File) private fileRepository: Repository<File>,
-                private metaService: MetaService) {}
-
-    async createFile(dtoFile: CreateFileDto, dtoMeta: CreateMetaDto): Promise<File> {
-        const file = this.fileRepository.create(dtoFile);
-        const metaFile = await this.metaService.createMeta(dtoMeta);
-        file.meta = metaFile;
-        return await this.fileRepository.save(file);
+                private metaService: MetaService) {
     }
 
+    async uploadFiles(files: Express.Multer.File[], newDtoFiles: NewFileDto[], dtoMetas: CreateMetaDto[]): Promise<File[]> {
+        const createdFiles: File[] = [];
+
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            const newDtoFile = newDtoFiles[i];
+            const dtoMeta = dtoMetas[i];
+
+            try {
+                const newFileName = `original-${Date.now()}-${uuid.v4()}.${file.originalname}`;
+
+                // Saving the original file
+                const originalFilePath = path.join(__dirname, '..', 'uploads', newFileName);
+                const relativeOriginalPath = '/uploads/' + newFileName;
+                await fs.promises.writeFile(originalFilePath, file.buffer);
+
+                // Creating and saving src
+                const srcBuffer = await sharp(file.buffer)
+                    .resize({
+                        width: 300,
+                        height: 250,
+                        fit: sharp.fit.inside,
+                        withoutEnlargement: true
+                    })
+                    .jpeg({ quality: 80 })
+                    .toBuffer();
+
+                const srcFileName = `src-${newFileName}`;
+                const srcFilePath = path.join(__dirname, '..', 'uploads', srcFileName);
+                const relativeSrcPath = '/uploads/' + srcFileName;
+                await fs.promises.writeFile(srcFilePath, srcBuffer);
+
+                // Creating and saving thumbnail
+                const thumbnailBuffer = await sharp(file.buffer)
+                    .resize({
+                        width: 100,
+                        height: 100,
+                        fit: sharp.fit.inside,
+                        withoutEnlargement: true
+                    })
+                    .jpeg({ quality: 80 })
+                    .toBuffer();
+
+                const thumbnailFileName = `thumbnail-${newFileName}`;
+                const thumbnailFilePath = path.join(__dirname, '..', 'uploads', thumbnailFileName);
+                const relativeThumbnailPath = '/uploads/' + thumbnailFileName;
+                await fs.promises.writeFile(thumbnailFilePath, thumbnailBuffer);
+
+                // Creating and saving previews
+                const previewBuffer = await sharp(file.buffer)
+                    .resize({
+                        width: 1000,
+                        height: 1000,
+                        fit: sharp.fit.inside,
+                        withoutEnlargement: true
+                    })
+                    .jpeg({ quality: 80 })
+                    .toBuffer();
+
+                const previewFileName = `preview-${newFileName}`;
+                const previewFilePath = path.join(__dirname, '..', 'uploads', previewFileName);
+                const relativePreviewPath = '/uploads/' + previewFileName;
+                await fs.promises.writeFile(previewFilePath, previewBuffer);
+
+                // Saving file information to a database
+                const newCreateFileDto = new CreateFileDto({
+                    title: newDtoFile.title,
+                    titleLong: newDtoFile.titleLong,
+                    alt: newDtoFile.alt,
+                    description: newDtoFile.description,
+                    original: relativeOriginalPath,
+                    preview: relativePreviewPath,
+                    src: relativeSrcPath,
+                    thumbnail: relativeThumbnailPath
+                });
+
+                const createFile = this.fileRepository.create(newCreateFileDto);
+                const metaFile = await this.metaService.createMeta(dtoMeta);
+                createFile.meta = metaFile;
+                const savedFile = await this.fileRepository.save(createFile);
+                createdFiles.push(savedFile);
+            } catch (error) {
+                console.error('Error during file upload:', error);
+            }
+        }
+        return createdFiles;
+    }
+
+
+
     async getFileById(id: number): Promise<File> {
-        return await this.fileRepository.findOne({ where: { id }, relations: ['meta'] });
+        return await this.fileRepository.findOne({where: {id}, relations: ['meta']});
     }
 
     async deleteFileById(id: number) {
-        return await this.fileRepository.delete( { id } )
+        return await this.fileRepository.delete({id})
     }
 
     async updateFileById(updateFileDto: UpdateFileDto, updateMetaDto: UpdateMetaDto): Promise<File> {
-        const existingFile = await this.fileRepository.findOne({ where: {id: updateFileDto.id}, relations: ['meta'] });
+        const existingFile = await this.fileRepository.findOne({where: {id: updateFileDto.id}, relations: ['meta']});
         if (!existingFile) {
             return null;
         }
